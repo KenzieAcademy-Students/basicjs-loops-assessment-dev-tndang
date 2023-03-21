c6af04396359685813f90efab768d888211d2cf5
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-03-21 12:38:16 -0400

    Completed Step One creating working functions to determine the results needed for Analyzing the Array.

--- code.js
+++ code.js
@@ -6,6 +6,96 @@ let numbers = [
     179, 535, 940, 472,
 ];
 
+let largestNumber = function() {
+    let highestNumber = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber > highestNumber) {
+            highestNumber = currentNumber;
+        }
+    }
+    console.log(`The largest number in the array is ${highestNumber}!`);
+    return highestNumber;
+}
+
+let smallestNumber = function() {
+    let lowestNumber = 1000;
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber < lowestNumber) {
+            lowestNumber = currentNumber;
+        }
+    }
+    console.log(`The smallest number in the array is ${lowestNumber}!`);
+    return lowestNumber;
+}
+
+let sum = function() {
+    let result = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        result += numbers[index];
+    }
+    console.log(`The sum of the array is ${result}!`);
+    return result;
+}
+
+let average = function() {
+    let sum = 0;
+    let result;
+    for (let index = 0; index < numbers.length; index++) {
+        sum += numbers[index];
+        if (numbers[index] >= numbers.length) {
+            result = sum / numbers.length;
+        }
+    }
+    console.log(`The average of the array is ${result}!`);
+    return result;
+}
+
+let evenNumbers = function() {
+    let evenArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber % 2 === 0) {
+            evenArray.push(currentNumber);
+        }
+    }
+    console.log(`The even numbers in the array are ${evenArray}!`);
+    return evenArray;
+}
+
+let oddNumbers = function() {
+    let oddArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber % 2 !== 0) {
+            oddArray.push(currentNumber);
+        }
+    }
+    console.log(`The odd numbers in the array are ${oddArray}!`);
+    return oddArray;
+}
+
+let eightDivisible = function() {
+    let divEightArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber % 8 === 0) {
+            divEightArray.push(currentNumber);
+        }
+    }
+    console.log(`The numbers divisible by 8 in the array are ${divEightArray}!`);
+    return divEightArray;
+}
+
+largestNumber();
+smallestNumber();
+sum();
+average();
+evenNumbers();
+oddNumbers();
+eightDivisible();
+
 document.write("<h1>Array Analyzer</h1>");
 let displayNumbers = function() { // This function will display the array to the DOM
     return numbers;
