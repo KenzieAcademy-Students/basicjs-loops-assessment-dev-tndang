2bd7942234569f7c548a81911675535a1b06ed4d
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-03-21 19:24:38 +0000

    deploy: 97c988da02aed874ea241ccbe2e8d0deadc922cb

--- code.js
+++ code.js
@@ -6,6 +6,106 @@ let numbers = [
     179, 535, 940, 472,
 ];
 
+let largestNumber = function() { // This function returns the largest number in the array
+    let highestNumber = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber > highestNumber) {
+            highestNumber = currentNumber;
+        }
+    }
+    console.log(`The largest number in the array is ${highestNumber}!`);
+    return highestNumber;
+}
+
+let smallestNumber = function() { // This function returns the smallest number in the array
+    let lowestNumber = 1000;
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber < lowestNumber) {
+            lowestNumber = currentNumber;
+        }
+    }
+    console.log(`The smallest number in the array is ${lowestNumber}!`);
+    return lowestNumber;
+}
+
+let sum = function() { // This function returns the total sum of all the numbers in the array
+    let result = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        result += numbers[index];
+    }
+    console.log(`The sum of the array is ${result}!`);
+    return result;
+}
+
+let average = function() { // This function returns the average value from the array
+    let sum = 0;
+    let result;
+    for (let index = 0; index < numbers.length; index++) {
+        sum += numbers[index];
+        if (numbers[index] >= numbers.length) {
+            result = sum / numbers.length;
+        }
+    }
+    console.log(`The average of the array is ${result}!`);
+    return result;
+}
+
+let evenNumbers = function() { // This function returns a new array with only the even numbers from the numbers[] array
+    let evenArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber % 2 === 0) {
+            evenArray.push(currentNumber);
+        }
+    }
+    console.log(`The even numbers in the array are ${evenArray}!`);
+    return evenArray;
+}
+
+let oddNumbers = function() { // This function returns a new array with only the odd numbers from the numbers[] array
+    let oddArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber % 2 !== 0) {
+            oddArray.push(currentNumber);
+        }
+    }
+    console.log(`The odd numbers in the array are ${oddArray}!`);
+    return oddArray;
+}
+
+let eightDivisible = function() { // This function returns a new array with only the numbers divisible by 8 from the numbers[] array
+    let divEightArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        let currentNumber = numbers[index];
+        if (currentNumber % 8 === 0) {
+            divEightArray.push(currentNumber);
+        }
+    }
+    console.log(`The numbers divisible by 8 in the array are ${divEightArray}!`);
+    return divEightArray;
+}
+
+let analyzerStings = [ // This array holds all the string results for the Array Analyzer
+    `Largest Number: ${largestNumber()}`,
+    `Smallest Number: ${smallestNumber()}`,
+    `Sum: ${sum()}`,
+    `Average Value: ${average()}`,
+    `Even Numbers: ${evenNumbers()}`,
+    `Odd Numbers: ${oddNumbers()}`,
+    `Numbers divisible by 8: ${eightDivisible()}`
+];
+
+let analyzerListHTML = function() { // This function lists out all the strings in the analyzerStings[] Array in HTML
+    let stringResult = "";
+    for (let index = 0; index < analyzerStings.length; index++) {
+        stringResult += `<li>${analyzerStings[index]}</li>`;
+    }
+    return stringResult;
+}
+
 document.write("<h1>Array Analyzer</h1>");
 let displayNumbers = function() { // This function will display the array to the DOM
     return numbers;
@@ -20,6 +120,7 @@ document.write("<ul>");
 //     document.write("<li>Second Number: " + numbers[1] + "</li>");
 
 // Write your code here 👇
+document.write(`${analyzerListHTML()}`);
 
 
 
